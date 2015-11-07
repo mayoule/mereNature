adaa310f3d1896c8e1c9207e9789b68184cf348e
Loic Messal <loic.messal@orange.fr>
2015-11-07 21:56:56 +0100

    Modification code nouvelle page et texte dans footer

--- php/head.php
+++ php/head.php
@@ -12,7 +12,7 @@
         </div>
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
+            <li class="active"><a href="./code_base_nouvelle_page.php">Home</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#contact">Contact</a></li>
           </ul>
