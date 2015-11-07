cb09e39c249065246e1a4734c0a3c2adc61aa957
hbaltz <hugo.baltz@ensg.eu>
2015-11-07 22:42:48 +0100

    test et envoi
    replissage de la base avec formulaire

--- php/test.php
+++ php/test.php
@@ -1,6 +1,6 @@
 <?php include("doctype.php")?>
-        <title>Nom de la page
-        </title>
+        <title>Nom de la page</title>
+    
     </head>
     <body>
 		
@@ -14,11 +14,39 @@
 		
 
         <div id="main"><h1>Test</h1>
-            <?php     
-                $sql = "INSERT INTO inscrits (nom,prenom,age,adresse) VALUES ('Jzan','Lui',42,'Paname hein');";
-                $pdo->query($sql);
+            <p>test</p>
+            <fieldset>
+                <form method='get' action='envoi_test.php'>
+                    <div >
+                        <label for="nom">Nom</label>
+                        <input type="nom"  id="nom" placeholder="Nom" name='nom' >
+                    </div>
+                   <div class="form-group">
+                        <label for="prenom">Prenom</label>
+                        <input type="prenom" class="form-control" id="prenom" placeholder="Prenom" name='prenom'>
+                        </div>
+                    <div class="form-group">
+                        <label for="age">Age</label>
+                        <input type="age" class="form-control" id="age" placeholder="Age" name='age'>
+                    </div>
+                    <div class="form-group">
+                        <label for="adresse">Adresse</label>
+                        <input type="adresse" class="form-control" id="adresse" placeholder="Adresse" name='adresse'>
+                        </div> 
+                    <button type="submit"  value='Valider'>Submit</button>
+                </form>
+            </fieldset>
+
+
+            
+
+            </div>
+            <?php   
+
+               // $sql = "INSERT INTO inscrits (nom,prenom,age,adresse) VALUES ('Jzan','Lui',42,'Paname hein');";
+               // $pdo->query($sql);
              ?>
-        </div>
+        
         <?php include("footer.php");?>
     </body>
 </html>
