932f60a97971a6285664649b87b899fa4deb7d8f
hbaltz <hugo.baltz@ensg.eu>
2015-11-08 16:01:06 +0100

    creer ta nnture

--- php/cree_ta_nature.php
+++ php/cree_ta_nature.php
@@ -8,8 +8,13 @@
         <?php include("menu.php"); ?>
 		<?php include("pub.php"); ?>
 		<?php include("onglet.php"); ?>
-		<img src="./image/slogan.png" alt="Ce qui nous unis" width=300px>
-		<img src="./image/logo_cloud_fini.png" alt="Tous ensemble" width=450px>
+
+        <table>
+               <tr> <img src="./image/slogan.png" alt="Ce qui nous unis" width=300px> </tr>
+               <tr> <img src="./image/logo_cloud_fini.png" alt="Tous ensemble" width=450px> </tr>
+
+		</table>
+		
 
         <div id="main">
 		
