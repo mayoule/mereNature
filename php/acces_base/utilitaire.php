36845f504d0fdc1ddf382fd2a8f2172be49b2446
mayoule <mayeul.lalanne@ensg.eu>
2015-11-08 07:40:31 +0100

    ajout methodes creergroupes creer projet

--- php/acces_base/utilitaire.php
+++ php/acces_base/utilitaire.php
@@ -86,4 +86,187 @@ function ajoutCompetence($pdo,$nom) {
     $pdo->query($sql);
     echo"<p>Inscritpion réussi</p>";	
 }
+
+
+<?php
+try {
+	$pdo = new PDO('mysql:host=localhost;dbname=mernat', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+} catch(Exception $e) {
+	echo 'Echec de la connexion à la base de données';
+	exit();
+}
+function __autoload($class_name) {
+    include 'php/class/' . $class_name . '.php';
+}
+
+function remplirInscrit($pdo,$nom,$prenom,$age,$adresse) {
+    $sql = "INSERT INTO inscrits (nom,prenom,age,adresse) VALUES ('".$nom."','".$prenom."',".$age.",'".$adresse."');";
+    $pdo->query($sql);
+    echo"<p>Inscritpion réussi</p>";
+}
+
+
+
+
+
+ function CreerProjet($pdo,$nom,$adresse,$description,$fond_necessaires,$fond_actuels,$date_debut,$chef_de_projet,$motCle,$skill_useful) {	
+
+ 	echo "on rentre ds la fonction";
+ 	//recuperation des donnes du formulaire
+
+	$motCle = preg_split("/[\s,]+/", $_GET['motCle']);
+	$skill_useful=preg_split("/[\s,]+/", $_GET['competence']);
+
+
+	//$sql = "INSERT INTO projets (nom, adresse, description, fond_necessaires, fond_actuels , date_debut, chef_de_projet) VALUES ('".$nom."','".$adresse."','".$description."',".$fond_necessaires.",".$fond_actuels.",'".$date_debut."','".$chef_de_projet."')";
+	//$request = $pdo->query($sql);
+	//$donnees = $request->fetch();
+
+
+	//identifiant de la competence
+	$sth = $pdo->prepare("INSERT INTO projets (nom, adresse, description, fond_necessaires, fond_actuels , date_debut, chef_de_projet) VALUES ('".$nom."','".$adresse."','".$description."',".$fond_necessaires.",".$fond_actuels.",'".$date_debut."','".$chef_de_projet."')");
+	$sth ->execute();
+	$stmt = $pdo->query("SELECT LAST_INSERT_ID()");
+	$result= $stmt->fetch(PDO::FETCH_ASSOC);
+	$id_pro= $result['LAST_INSERT_ID()'];
+
+	//MOT CLES DU PROJET
+	//on parcourt le tableau des mots cle
+	foreach ($motCle as $attribut) {
+		//mots cles existants ?
+		echo $attribut;
+
+		$exist=$pdo->prepare("SELECT * FROM competences WHERE nom='".$attribut."'");
+		$exist->execute();
+		$result1= $exist->fetch(PDO::FETCH_ASSOC);
+		//$result3=$result1['LAST_INSERT_ID()'];
+
+
+		//si la competence n existe pas			
+		if ($result1){
+			echo "deja fait!";
+			$id_m = $result1['id_com'];
+		}else
+			{
+			echo "nul";
+			//modif competence
+			$sql2 = $pdo->prepare("INSERT INTO competences (nom)  VALUES ('".$attribut."')");
+			$sql2->execute();
+
+			$stmt = $pdo->query("SELECT LAST_INSERT_ID() ");
+			$result3= $stmt->fetch(PDO::FETCH_ASSOC);
+			$id_m= $result3['LAST_INSERT_ID()'];
+
+			
+		}
+		$sql3 = $pdo->prepare("INSERT INTO mots_projets (idm, idp)  VALUES (".$id_m.", ".$id_pro.")");	
+		$sql3->execute();
+	}
+
+	//COMPETENCES DU PROJETs
+	//on parcourt le tableau des competences
+	foreach ($skill_useful as $attribut) {
+		//mots cles existants ?
+		echo $attribut;
+
+		$exist=$pdo->prepare("SELECT * FROM competences WHERE nom='".$attribut."'");
+		$exist->execute();
+		$result5= $exist->fetch(PDO::FETCH_ASSOC);
+		//$result3=$result1['LAST_INSERT_ID()'];
+		//echo $result3;
+
+
+
+
+
+		//si la competence n existe pas			
+		if ($result5){
+			echo "deja fait!";
+			$id_m1 = $result5['id_com'];
+		}else
+			{
+			//modif competence
+			echo "c est nul";
+			$sql4 = $pdo->prepare("INSERT INTO competences (nom)  VALUES ('".$attribut."')");
+			$sql4->execute();
+
+			$stmt = $pdo->query("SELECT LAST_INSERT_ID() ");
+			$result2= $stmt->fetch(PDO::FETCH_ASSOC);
+			$id_m1= $result2['LAST_INSERT_ID()'];
+			echo $id_m1;
+
+
+		}
+		$sql3 = $pdo->prepare("INSERT INTO competences_projets (idc, idp)  VALUES (".$id_m1.", ".$id_pro.")");	
+		$sql3->execute();		
+	}	
+
+}
+
+
+function CreerGroupe($pdo,$nom,$adresse,$description,$createur,$motCle) {	
+
+ 	echo "on rentre ds la fonction";
+ 	//recuperation des donnes du formulaire
+
+	$motCle = preg_split("/[\s,]+/", $_GET['motCle']);
+
+
+	//$sql = "INSERT INTO projets (nom, adresse, description, fond_necessaires, fond_actuels , date_debut, chef_de_projet) VALUES ('".$nom."','".$adresse."','".$description."',".$fond_necessaires.",".$fond_actuels.",'".$date_debut."','".$chef_de_projet."')";
+	//$request = $pdo->query($sql);
+	//$donnees = $request->fetch();
+
+
+	//identifiant de la competence
+	$sth = $pdo->prepare("INSERT INTO groupes (nom, adresse, description, createur) VALUES ('".$nom."','".$adresse."','".$description."','".$createur."')");
+	$sth ->execute();
+	$stmt = $pdo->query("SELECT LAST_INSERT_ID()");
+	$result= $stmt->fetch(PDO::FETCH_ASSOC);
+	$id_pro= $result['LAST_INSERT_ID()'];
+
+	//MOT CLES DU PROJET
+	//on parcourt le tableau des mots cle
+	foreach ($motCle as $attribut) {
+		//mots cles existants ?
+		echo $attribut;
+
+		$exist=$pdo->prepare("SELECT * FROM competences WHERE nom='".$attribut."'");
+		$exist->execute();
+		$result1= $exist->fetch(PDO::FETCH_ASSOC);
+		//$result3=$result1['LAST_INSERT_ID()'];
+
+
+		//si la competence n existe pas			
+		if ($result1){
+			echo "deja fait!";
+			$id_m = $result1['id_com'];
+		}else
+			{
+			echo "nul";
+			//modif competence
+			$sql2 = $pdo->prepare("INSERT INTO competences (nom)  VALUES ('".$attribut."')");
+			$sql2->execute();
+
+			$stmt = $pdo->query("SELECT LAST_INSERT_ID() ");
+			$result3= $stmt->fetch(PDO::FETCH_ASSOC);
+			$id_m= $result3['LAST_INSERT_ID()'];
+
+			
+		}
+		$sql3 = $pdo->prepare("INSERT INTO competences_groupes (idc, idg)  VALUES (".$id_m.", ".$id_pro.")");	
+		$sql3->execute();
+	}
+
+	
+
+}
+
+
+
+
+
+
+?>
+
+
 ?>
---
+++ php/creationGroupes_projets.php
@@ -0,0 +1,70 @@
+<?php include("doctype.php")?>
+        <title>Nom de la page</title>
+    
+    </head>
+    <body>
+		
+        <?php include("head.php"); ?>
+        <?php include("menu.php"); ?>
+		<?php include("onglet.php"); ?>
+        <?php include("acces_base/utilitaire.php"); ?>
+
+
+
+        <div id="main"><h1>Proposer Projet</h1>
+            <p>test</p>
+            <fieldset>
+                <form method='get' action='envoi_test.php'>
+                    <div >
+                        <label for="nom">Nom</label>
+                        <input type="nom"  id="nom" placeholder="Nom" name='nom' value='qsdf'>
+                    </div>
+                   <div class="form-group">
+                        <label for="adresse">adresse</label>
+                        <input type="adresse"  id="adresse" placeholder="adresse" name='adresse' value='bla'>
+                        </div>
+                    <div class="form-group">
+                        <label for="description">description</label>
+                        <input type="description"  id="description" placeholder="description" name='description' value='bla'>
+                    </div>
+                    <div class="form-group">
+                        <label for="fond_necessaires">fond necessaires</label>
+                        <input type="fond_necessaires"  id="fond_necessaires" placeholder="fond_necessaires" name='fond_necessaires' value=5>
+                    </div> 
+                    <div class="form-group">
+                        <label for="fond_actuels">fond actuels</label>
+                        <input type="fond_actuels"  id="fond_actuels" placeholder="fond_actuels" name='fond_actuels' value=5>
+                    </div>
+                    <div class="form-group">
+                        <label for="date_debut">date de debut</label>
+                        <input type="date_debut"  id="date_debut" placeholder="date_debut" name='date_debut' value='1992-04-11'>
+                    </div>
+                    <div class="form-group">
+                        <label for="chef_de_projet">chef de projet</label>
+                        <input type="chef_de_projet"  id="chef_de_projet" placeholder="chef_de_projet" name='chef_de_projet' value='qsdf'>
+                    </div>
+                    <div class="form-group">
+                        <label for="motCle">mots Clés : mot-clé1,mot-clé2</label>
+                        <input type="motCle"  id="motCle" placeholder="motCle" name='motCle' value='qsdf'>
+                    </div>
+                    <div class="form-group">
+                        <label for="competence">Compétences requises</label>
+                        <input type="competence"  id="competence" placeholder="competence" name='competence' value='qsdf'>
+                    </div>
+
+                    <button type="submit"  value='Valider'>Submit</button>
+                </form>
+            </fieldset>
+
+           
+
+            </div>
+            <?php   
+
+               // $sql = "INSERT INTO inscrits (nom,prenom,age,adresse) VALUES ('Jzan','Lui',42,'Paname hein');";
+               // $pdo->query($sql);
+             ?>
+        
+        <?php include("footer.php");?>
+    </body>
+</html>
