a75ca9833c8fa0b23f834acc54c14e359bac4289
hbaltz <hugo.baltz@ensg.eu>
2015-11-08 12:45:20 +0100

    maj css image a propos

--- php/apropos.php
+++ php/apropos.php
@@ -1,5 +1,5 @@
 <?php include("doctype.php")?>
-        <title>Nouvelle Page
+        <title>A propos de nous
         </title>
     </head>
     <body>
@@ -17,25 +17,36 @@
             <p>A l'origine de l'aventure Fungi se trouvent six étudiants, aux profils différents,
             lors du challenge Inter école organisé à Paris par le C3 lors de la COP21.</p>
             
-            <div class="row">
-                <div class="col-md-4">
-                    <img  class="IMG" src="image/orga/edouard.png" alt="Edouard" width=250em>
-                </div>
-                <div class="col-md-8">
-                    <p>Je suis étudiant depuis maintenant quatre ans dans une école spécialisée dans
+            <table>
+              <tr>
+                <td><img  class="IMG" src="image/orga/edouard.png" alt="Edouard" width=250em></td>
+                <td>Je suis étudiant depuis maintenant quatre ans dans une école spécialisée dans
                     l'entreprenariat. J'ai consyayé que les idées ne manquaient pas. Mais leur est
                     développement est limité par les aspects techniques. C'est là que notre site entre
-                    en jeu.</p>
-                </div>
+                    en jeu.</td>
+              </tr>
+              <tr>
+                <td><img  class="IMG" src="image/orga/erwan.png" alt="Erwan" width=250em></td>
+                <td>...</td>
+              </tr>
+              <tr>
+                <td><img  class="IMG" src="image/orga/armance.png" alt="Armance" width=250em></td>
+                <td>...</td>
+              </tr>
+              <tr>
+                <td><img  class="IMG" src="image/orga/loic.png" alt="Loic" width=250em></td>
+                <td>...</td>
+              </tr>
+              <tr>
+                <td><img  class="IMG" src="image/orga/mayeul.png" alt="Mayeul" width=250em></td>
+                <td>...</td>
+              </tr>
+              <tr>
+                <td><img  class="IMG" src="image/orga/hugo.png" alt="Hugo" width=250em></td>
+                <td>...</td>
+              </tr>
+            </table>
             
-                <div class="col-md-4">
-                    <img  class="IMG" src="image/orga/erwan.png" alt="Erwan" width=250em>
-
-                </div>
-                <div class="col-md-8">
-                    <p></p>
-                </div>
-            </div>
 
             <p>Rassemblés autour d'un même projet: faire naître la nature n'importe où.
             Fort de cette expérience de partage des compétences, et pour répondre à un besoin du
