871a54ece316cb478a7b31169e23e004004298fd
hbaltz <hugo.baltz@ensg.eu>
2015-11-08 12:31:07 +0100

    avancement apropos

--- php/apropos.php
+++ php/apropos.php
@@ -14,16 +14,36 @@
 
         <div id="main">
 		
-        A l'origine de l'aventure Fungi se trouvent six étudiants, aux profils différents,
-        lors du challenge Inter école organisé à Paris par le C3 lors de la COP21.
-        *****organigramme********
-        Rassemblés autour d'un même projet: faire naître la nature n'importe où.
-        Fort de cette expérience de partage des compétences, et pour répondre à un besoin du
-        Biofablab de Toulouse, nous avons décidé de créer cette plateforme de Biomakers, où
-        les compétences, les ressources et les envies de chacun peuvent s'exprimer.
-        L'idée est de sensibiliser et de mettre en relation les personnes. C'est de faire
-        comprendre aux citoyens que tous peuvent être acteurs, à leur manière, dans la lutte
-        contre le réchauffement climatique et développer la nature
+            <p>A l'origine de l'aventure Fungi se trouvent six étudiants, aux profils différents,
+            lors du challenge Inter école organisé à Paris par le C3 lors de la COP21.</p>
+            
+            <div class="row">
+                <div class="col-md-4">
+                    <img  class="IMG" src="image/orga/edouard.png" alt="Edouard" width=250em>
+                </div>
+                <div class="col-md-8">
+                    <p>Je suis étudiant depuis maintenant quatre ans dans une école spécialisée dans
+                    l'entreprenariat. J'ai consyayé que les idées ne manquaient pas. Mais leur est
+                    développement est limité par les aspects techniques. C'est là que notre site entre
+                    en jeu.</p>
+                </div>
+            
+                <div class="col-md-4">
+                    <img  class="IMG" src="image/orga/erwan.png" alt="Erwan" width=250em>
+
+                </div>
+                <div class="col-md-8">
+                    <p></p>
+                </div>
+            </div>
+
+            <p>Rassemblés autour d'un même projet: faire naître la nature n'importe où.
+            Fort de cette expérience de partage des compétences, et pour répondre à un besoin du
+            Biofablab de Toulouse, nous avons décidé de créer cette plateforme de Biomakers, où
+            les compétences, les ressources et les envies de chacun peuvent s'exprimer.
+            L'idée est de sensibiliser et de mettre en relation les personnes. C'est de faire
+            comprendre aux citoyens que tous peuvent être acteurs, à leur manière, dans la lutte
+            contre le réchauffement climatique et développer la nature</p>
 
 
         </div>
